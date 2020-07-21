f996695e542e51425e3794914523940d62ac3fc2
Jason Keller <keller.jason95@gmail.com>
2020-07-21 16:12:24 -0600

    Functionality update'

--- rps/app.js
+++ rps/app.js
@@ -1,25 +1,19 @@
 const choices = ["Rock", "Paper", "Scissors"];
 const btn = document.querySelector("button");
+const playerSelection = prompt("Choose either \"Rock\", \"Paper\", or \"Scissors\". Inputs are not case-sensitive");
+const computerSelection = computerPlay();
 
 btn.addEventListener("click", function(){
     console.log("functionality is coming soon.");
 });
 
-
-
-
-
+// This is where the game is played
+function playRound(playerSelection, computerSelection) {
+    
+}
 
 // This will return what the computer chooses at random
 function computerPlay() {
     let computerChoice = choices[Math.floor(Math.random() * choices.length)]
     return computerChoice;
-}
-
-// This is where the game is played
-function playRound(playerSelection, computerSelection) {
-    // This first prompts the player to make a choice.
-    playerSelection = prompt("Choose either \"Rock\", \"Paper\", or \"Scissors\"");
-    return playerSelection;
-    // If the player tries to choose anything other than the three options, it throws another prompt.
 }
